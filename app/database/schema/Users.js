8d64091c315056aa6550cce07514f19cdc0f8e34
lx812833 <lx812833@163.com>
2019-10-14 21:26:21 +0800

    个人参数schema

--- app/database/schema/Users.js
+++ app/database/schema/Users.js
@@ -19,6 +19,39 @@ const userSchema = new Schema({
         type: String,
         required: true,
         select: false // 默认返回，设置为false后不返回密码字段
+    },
+    avatar_url: {
+        type: String
+    },
+    gender: {
+        type: String,
+        enum: ['male', 'female'],  // 枚举性别
+        default: 'male',
+        required: true
+    },
+    headline: {
+        type: String
+    },
+    locations: {
+        type: [{ type: String }]   // 字符串数组
+    },
+    business: {
+        type: String
+    },
+    employments: {
+        type: [{
+            company: { type: String },
+            job: { type: String }
+        }]
+    },
+    educations: {
+        type: [{
+            school: { type: String },
+            major: { type: String },
+            diploma: { type: Number, enum: [1, 2, 3, 4, 5] },
+            entrance_year: { type: Number },
+            graduation_year: { type: Number }
+        }]
     }
 })
 
