d53a40b9968c37b30f918c119a284587dc7418c3
lx812833 <lx812833@163.com>
2020-02-18 21:42:38 +0800

    修改

--- app/controllers/file.js
+++ app/controllers/file.js
@@ -2,6 +2,19 @@ const path = require("path")
 const fse = require("fs-extra")
 
 const UPLOAD_DIR = path.resolve(__dirname, "..", "target") // 当前目录下的上层目录target
+const SIZE = 1 * 1024 * 1024; // 1M
+
+const pipeStream = (path, writeStream) => {
+  new Promise(resolve => {
+    const readStream = fse.createReadStream(path)
+    // 创建文件可读流
+    readStream.on("end", () => {
+      fse.unlinkSync(path) // 删除文件
+      resolve()
+    })
+    readStream.pipe(writeStream)
+  })
+}
 
 class bigFileUpload {
   async verify(ctx) {
@@ -27,11 +40,46 @@ class bigFileUpload {
     }
   }
   async chunkUpload(ctx) {
-    console.log("切块上传", ctx.request.body)
+    const { chunk } = ctx.request.files
+    const { hash, fileName, fileHash } = ctx.request.body
+    const fileName_ = fileName.slice(fileName.lastIndexOf("."), fileName.length)
+    const filePath = path.resolve(UPLOAD_DIR, `${fileHash}${fileName_}`)
+    const chunkDir = path.resolve(UPLOAD_DIR, fileHash) // 目录地址
+    if (fse.existsSync(filePath)) {
+      ctx.body = {
+        code: 200,
+        data: {
+          message: "File exist"
+        }
+      }
+      return
+    }
+    if (!fse.existsSync(chunkDir)) {
+      await fse.mkdirs(chunkDir)
+    }
+    await fse.move(chunk.path, path.resolve(chunkDir, hash))
+
+    // 合并文件块
+    const chunkPaths = await fse.readdir(chunkDir)
+    chunkPaths.sort((a, b) => a.split("-")[1] - b.split("-")[1])
+
+    await Promise.all(
+      chunkPaths.map((chunkPath, index) => {
+        pipeStream(
+          path.resolve(chunkDir, chunkPath),
+          fse.createWriteStream(filePath, {
+            start: index * SIZE,
+            end: (index + 1) * SIZE
+          })
+        )
+      })
+    )
+    fse.rmdir(chunkDir) // 合并后删除保存切片的目录
+
     ctx.body = {
       code: 200,
       data: {
-        data: ctx.request.body
+        message: "Received file chunk"
       }
     }
   }
